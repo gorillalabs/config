61a0b133d10ad54717cbe93fae84a347b0d7a5f9
Dr. Christian Betz <cb@cypp.hamburg>
2017-11-27 11:30:51 +0100

    move to lein-v for project version management

--- project.clj
+++ project.clj
@@ -1,9 +1,17 @@
 (defproject
   gorillalabs/config
-  "1.0.4"
+  "0.0.0"
   :description "A Clojure config file handler"
   :scm {:name "git"
         :url  "https://github.com/gorillalabs/config"}
+  :deploy-repositories [["releases" :clojars]]
+  
+  :plugins [[com.roomkey/lein-v "6.2.0"]]
+
+  :middleware [leiningen.v/version-from-scm
+               leiningen.v/add-workspace-data]
+
+
   :dependencies [[org.clojure/clojure "1.8.0"]
                  [org.clojure/tools.logging "0.3.1"]]
   :profiles {:dev {:dependencies   [[expectations "2.0.8"]]
@@ -15,4 +23,11 @@
   :aliases {"test" ["do" "clean" ["with-profiles" "+test" "expectations"]]
             }
   :javac-options ["-target" "1.6" "-source" "1.6"]
-  :jvm-opts ["-Dfile.encoding=utf-8"])
+  :jvm-opts ["-Dfile.encoding=utf-8"]
+  
+    :release-tasks [["vcs" "assert-committed"]
+                  ["v" "update"]                            ;; compute new version & tag it
+                  ["monolith" "each" "deploy"]
+                  ["vcs" "push"]
+                  ]
+  )
